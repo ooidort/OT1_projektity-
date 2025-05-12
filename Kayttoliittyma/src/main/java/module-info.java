8192e73a993000f1a4c86245766997d6bd5b36c8
Osku Kinnunen <kinnunenosku@hotmail.com>
2025-05-12 19:25:27 +0300

    SQL-tietokantaan kirjoittamista varten luotu olioluokat jokaiselle tietokannan kentälle, sekä uudelleen paketoitu projektin rakennetta siten että käyttöliittymäelementit ovat omassa paketissaan ja tietokantaelementit omassa

--- Kayttoliittyma/src/main/java/module-info.java
+++ Kayttoliittyma/src/main/java/module-info.java
@@ -2,10 +2,13 @@ module com.ot1.kayttoliittyma {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires java.sql;
 
 
     opens com.ot1.kayttoliittyma to javafx.fxml;
     exports com.ot1.kayttoliittyma;
     exports src.main.java.com.ot1.kayttoliittyma;
     opens src.main.java.com.ot1.kayttoliittyma to javafx.fxml;
+    exports tietokantahaut;
+    opens tietokantahaut to javafx.fxml;
 }
---
+++ Kayttoliittyma/src/main/java/tietokantahaut/LaskuLuokka.java
@@ -0,0 +1,82 @@
+package tietokantahaut;
+
+import java.sql.Timestamp;
+
+public class LaskuLuokka {
+
+    private int laskuID;
+    private Timestamp erapaiva_alku;
+    private Timestamp erapaiva_loppu;
+    private Double summa;
+
+    public LaskuLuokka(int laskuID, Timestamp erapaiva_alku, Timestamp erapaiva_loppu, Double summa) {
+        this.laskuID = laskuID;
+        this.erapaiva_alku = erapaiva_alku;
+        this.erapaiva_loppu = erapaiva_loppu;
+        this.summa = summa;
+    }
+
+    /**
+     * Asettaa laskun eräpäivän alkupäivämäärän
+     * @param erapaiva_alku
+     */
+    public void setErapaiva_alku(Timestamp erapaiva_alku) {
+        this.erapaiva_alku = erapaiva_alku;
+    }
+
+    /**
+     * Asettaa viimeisen eräpäivän
+     * @param erapaiva_loppu
+     */
+    public void setErapaiva_loppu(Timestamp erapaiva_loppu) {
+        this.erapaiva_loppu = erapaiva_loppu;
+    }
+
+    /**
+     * Asettaa laskun pääavaimen
+     * @param laskuID
+     */
+    public void setLaskuID(int laskuID) {
+        this.laskuID = laskuID;
+    }
+
+    /**
+     * Asettaa laskun summan
+     * @param summa
+     */
+    public void setSumma(Double summa) {
+        this.summa = summa;
+    }
+
+    /**
+     * Hakee laskun summan
+     * @return
+     */
+    public Double getSumma() {
+        return summa;
+    }
+
+    /**
+     * Hakee laskun pääavaimen
+     * @return
+     */
+    public int getLaskuID() {
+        return laskuID;
+    }
+
+    /**
+     * Hakee laskun laskuttamiserän alkupäivämäärän
+     * @return
+     */
+    public Timestamp getErapaiva_alku() {
+        return erapaiva_alku;
+    }
+
+    /**
+     * Hakee laskun erääntymispäivän
+     * @return
+     */
+    public Timestamp getErapaiva_loppu() {
+        return erapaiva_loppu;
+    }
+}
