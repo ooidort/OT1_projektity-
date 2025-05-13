4208cb5b1056a7081236486629c10aadbcfb8308
eetu90 <eetu.nikkila01@gmail.com>
2025-05-13 16:11:01 +0300

    MajoitusController parannuksia

---
+++ Kayttoliittyma/src/main/java/com/ot1/kayttoliittyma/majoitusUIController.java
@@ -0,0 +1,22 @@
+package com.ot1.kayttoliittyma;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class majoitusUIController extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(com.ot1.kayttoliittyma.majoitusUIController.class.getResource("ui/majoitusVaraukset.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        primaryStage.setTitle("Majoitusvaraukset");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+}
